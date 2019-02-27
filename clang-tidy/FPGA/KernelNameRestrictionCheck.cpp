562a3814592f7d0540db1c45754102ac9e201a98
Frank Wanye <wanyef@charon.cs.vt.edu>
2019-02-27 17:13:25 -0500

    Added FPGA-kernel-name-restriction check
    Based on Guideline #4 from the "Guidelines for Naming the Kernel" in the Intel FPGA SDK for OPenCL Programming Guide
    Source: https://www.altera.com/documentation/mwh1391807965224.html#ewa1411503895045
    
    Matches on FunctionDeclarations, and checks whether or not the FileEntry for the source file in which the function is
    declared ends with "/kernel.cl".
    
    In order to not point towards the function itself, a SourceLocation is created for the kernel FileEntry line 1 column 1,
    and passed to the diag() function.

---
+++ clang-tidy/FPGA/KernelNameRestrictionCheck.cpp
@@ -0,0 +1,42 @@
+//===--- KernelNameRestrictionCheck.cpp - clang-tidy ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "KernelNameRestrictionCheck.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+using namespace clang::ast_matchers;
+
+namespace clang {
+namespace tidy {
+namespace FPGA {
+
+void KernelNameRestrictionCheck::registerMatchers(MatchFinder *Finder) {
+  // TranslationUnitDecl is the root of all ASTs
+  Finder->addMatcher(functionDecl().bind("function"), this);
+}
+
+void KernelNameRestrictionCheck::check(const MatchFinder::MatchResult &Result) {
+  // StackOverflow example: https://stackoverflow.com/a/25079453/5760608
+  const Decl *MatchedDecl = Result.Nodes.getNodeAs<Decl>("function");
+  ASTContext &Context = MatchedDecl->getASTContext();
+  const SourceManager &SrcManager = Context.getSourceManager();
+  const FileEntry *Entry = SrcManager.getFileEntryForID(
+    SrcManager.getFileID(
+      MatchedDecl->getBeginLoc()));
+  StringRef FileName = Entry->getName();
+  if (FileName.endswith_lower("/kernel.cl")) {
+    diag(SrcManager.translateLineCol(SrcManager.getFileID(MatchedDecl->getBeginLoc()),1,1),
+       "Naming your OpenCL kernel source file \"kernel.cl\" could cause compilation errors.");
+    // diag(MatchedDecl->getLocation(), "Naming your OpenCL kernel source file \"kernel.cl\" could cause compilation errors.");
+  }
+}
+
+} // namespace FPGA
+} // namespace tidy
+} // namespace clang
