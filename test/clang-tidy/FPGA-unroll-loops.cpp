adbcb2e5922a5086a92fc2a233284101470930c4
Frank Wanye <wanyef@charon.cs.vt.edu>
2019-04-08 16:49:28 -0400

    Added unit tests for FPGA-unroll-loops lint check

--- test/clang-tidy/FPGA-unroll-loops.cpp
+++ test/clang-tidy/FPGA-unroll-loops.cpp
@@ -1,4 +1,4 @@
-// RUN: %check_clang_tidy %s FPGA-unroll-loops %t -- -header-filter=.* "--" --include opencl-c.h -cl-std=CL1.2 -c
+// RUN: %check_clang_tidy %s FPGA-unroll-loops %t -- -config="{CheckOptions: [{key: "FPGA-unroll-loops.max_loop_iterations", value: 50}]}" -header-filter=.* "--" --include opencl-c.h -cl-std=CL1.2 -c
 
 // Inner loops should be unrolled
 __kernel void nested_simple_loops(__global int *A) {
@@ -111,7 +111,7 @@ __kernel void nested_simple_loops(__global int *A) {
 __kernel void unrolled_nested_simple_loops(__global int *A) {
     for (int i = 0; i < 1000; ++i) {
         #pragma unroll
-        for (int j = 0; j < 2000; ++j) {
+        for (int j = 0; j < 50; ++j) {
             A[0] += i + j;
         }
     }
@@ -119,7 +119,7 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
     for (int i = 0; i < 1000; ++i) {
         int j = 0;
         #pragma unroll
-        while (j < 2000) {
+        while (j < 50) {
             A[1] += i + j;
             j++;
         }
@@ -131,13 +131,13 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
         do {
             A[2] += i + j;
             j++;
-        } while (j < 2000);
+        } while (j < 50);
     }
 
     int i = 0;
     while (i < 1000) {
         #pragma unroll
-        for (int j = 0; j < 2000; ++j) {
+        for (int j = 0; j < 50; ++j) {
             A[3] += i + j;
         }
         i++;
@@ -147,7 +147,7 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
     while (i < 1000) {
         int j = 0;
         #pragma unroll
-        while (j < 2000) {
+        while (50 > j) {
             A[4] += i + j;
             j++;
         }
@@ -155,20 +155,20 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
     }
 
     i = 0;
-    while (i < 1000) {
+    while (1000 > i) {
         int j = 0;
         #pragma unroll
         do {
             A[5] += i + j;
             j++;
-        } while (j < 2000);
+        } while (j < 50);
         i++;
     }
 
     i = 0;
     do {
         #pragma unroll
-        for (int j = 0; j < 2000; ++j) {
+        for (int j = 0; j < 50; ++j) {
             A[6] += i + j;
         }
         i++;
@@ -178,7 +178,7 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
     do {
         int j = 0;
         #pragma unroll
-        while (j < 2000) {
+        while (j < 50) {
             A[7] += i + j;
             j++;
         }
@@ -192,8 +192,168 @@ __kernel void unrolled_nested_simple_loops(__global int *A) {
         do {
             A[8] += i + j;
             j++;
-        } while (j < 2000);
+        } while (j < 50);
         i++;
     } while (i < 1000);
+}
+
+__kernel void unrolled_nested_simple_loops_large_num_iterations(__global int *A) {
+    for (int i = 0; i < 1000; ++i) {
+        #pragma unroll
+        for (int j = 0; j < 51; ++j) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[0] += i + j;
+        }
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        int j = 0;
+        #pragma unroll
+        while (j < 51) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[1] += i + j;
+            j++;
+        }
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        int j = 0; 
+        #pragma unroll
+        do {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[2] += i + j;
+            j++;
+        } while (j < 51);
+    }
+
+    int i = 0;
+    while (i < 1000) {
+        #pragma unroll
+        for (int j = 0; j < 51; ++j) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[3] += i + j;
+        }
+        i++;
+    }
+
+    i = 0;
+    while (i < 1000) {
+        int j = 0;
+        #pragma unroll
+        while (51 > j) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[4] += i + j;
+            j++;
+        }
+        i++;
+    }
+
+    i = 0;
+    while (1000 > i) {
+        int j = 0;
+        #pragma unroll
+        do {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[5] += i + j;
+            j++;
+        } while (j < 51);
+        i++;
+    }
+
+    i = 0;
+    do {
+        #pragma unroll
+        for (int j = 0; j < 51; ++j) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[6] += i + j;
+        }
+        i++;
+    } while (i < 1000);
+
+    i = 0;
+    do {
+        int j = 0;
+        #pragma unroll
+        while (j < 51) {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[7] += i + j;
+            j++;
+        }
+        i++;
+    } while (i < 1000);
+
+    i = 0;
+    do {
+        int j = 0;
+        #pragma unroll
+        do {
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: This loop likely has a large number of iterations and thus cannot be fully unrolled. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+            A[8] += i + j;
+            j++;
+        } while (j < 51);
+        i++;
+    } while (i < 1000);
+}
+
+__kernel void fully_unrolled_unknown_bounds(int vectorSize) {
+    int someVector[101];
+
+    // There is no loop condition
+    #pragma unroll
+    for (;;) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    }
+
+    // Infinite loop
+    #pragma unroll
+    while (true) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    }
+
+    #pragma unroll
+    do {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    } while (true);
+
+    #pragma unroll
+    while (1 < 5) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    }
+
+    #pragma unroll
+    do {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    } while (1 < 5);
+
+    #pragma unroll
+    for (int i = 0; 1 < 5; ++i) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        someVector[i]++;
+    }
+
+    // Both sides are value-dependent
+    #pragma unroll
+    for (int i = 0; i < vectorSize; ++i) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    }
+
+    int j = 0;
+    #pragma unroll
+    while (j < vectorSize) {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    }
+    
+    #pragma unroll
+    do {
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: Full unrolling was requested, but loop bounds are not known. To partially unroll this loop, use the #pragma unroll <num> directive [FPGA-unroll-loops]
+        printf("%d", someVector);
+    } while (j < vectorSize);
 }
 // There are no fix-its for this check
