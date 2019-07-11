a455f4cf89b7cb467698f1ba89dcc77a22c5edd6
Frank Wanye <wanyef@charon.cs.vt.edu>
2019-07-11 11:02:19 -0400

    Added tests for warnings in loops

--- test/clang-tidy/FPGA-ID-dependent-backward-branch.cpp
+++ test/clang-tidy/FPGA-ID-dependent-backward-branch.cpp
@@ -4,18 +4,10 @@ typedef struct ExampleStruct {
   int IDDepField;
 } ExampleStruct;
 
-class ExampleClass {
-  public:
-  int IDDepField;
-  ExampleClass(int x) {
-    IDDepField = x;
-  }
-};
-
 void error() {
   // ==== Assignments ====
   int tid = get_local_id(0);
-// CHECK-MESSAGES: :[[@LINE-1]]:3: warning: assignment of ID-dependent variable 'tid' declared at {{(\/)?([^\/\0]+(\/)?)+}}:[[@LINE-1]]:3 [FPGA-ID-dependent-backward-branch]
+// CHECK-NOTES: :[[@LINE-1]]:3: note: assignment of ID-dependent variable 'tid' declared at {{(\/)?([^\/\0]+(\/)?)+}}:[[@LINE-1]]:3 [FPGA-ID-dependent-backward-branch]
 
   ExampleStruct Example;
   Example.IDDepField = get_local_id(0);
@@ -23,50 +15,71 @@ void error() {
 
   // ==== Inferred Assignments ====
   int tx = tid * get_local_size(0);
-// CHECK-MESSAGES: :[[@LINE-1]]:12: warning: Inferred assignment of ID-dependent value from ID-dependent variable 'tid' [FPGA-ID-dependent-backward-branch]
+// CHECK-NOTES: :[[@LINE-1]]:12: note: Inferred assignment of ID-dependent value from ID-dependent variable 'tid' [FPGA-ID-dependent-backward-branch]
   int tx2 = Example.IDDepField;
-// CHECK-MESSAGES: :[[@LINE-1]]:13: warning: Inferred assignment of ID-dependent value from ID-dependent member 'IDDepField' [FPGA-ID-dependent-backward-branch]
+// CHECK-NOTES: :[[@LINE-1]]:13: note: Inferred assignment of ID-dependent value from ID-dependent member 'IDDepField' [FPGA-ID-dependent-backward-branch]
 
   ExampleStruct Example2 = {
     tid * 2
   };
 
-  ExampleClass Example3(tid);
+  // ExampleClass Example3(tid);
   // Example2.IDDepField = Example.IDDepField * 2;
   // ==== Conditional Expressions ====
-
-
-  /** =========MAYBE DELETE?===========
-  for(int i = 0; i < 256; i++) {
-    if (i < tid) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:9: warning: Conditional inside loop is ID-dependent due to variable reference to 'tid' [FPGA-ID-dependent-backward-branch]
-    } 
-    if (i < get_global_id(0)) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:13: warning: Conditional inside loop is ID-dependent due to ID function call [FPGA-ID-dependent-backward-branch]
-    }
+  int accumulator = 0;
+  for (int i = 0; i < get_local_id(0); i++) {
+    // CHECK-NOTES: :[[@LINE-1]]:23: warning: Backward branch (for loop) is ID-dependent due to ID function call and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
   }
+
   int j = 0;
-  while (j < 256) {
-    if (j < tid) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:9: warning: Conditional inside loop is ID-dependent due to variable reference to 'tid' [FPGA-ID-dependent-backward-branch]
-    }
-    if (j < get_global_id(0)) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:13: warning: Conditional inside loop is ID-dependent due to ID function call [FPGA-ID-dependent-backward-branch]
-    }
-    j++;
+  while (j < get_local_id(0)) {
+    // CHECK-NOTES: :[[@LINE-1]]:14: warning: Backward branch (while loop) is ID-dependent due to ID function call and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
   }
+
   do {
-    if (j < tid) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:9: warning: Conditional inside loop is ID-dependent due to variable reference to 'tid' [FPGA-ID-dependent-backward-branch]
-    }
-    if (j < get_global_id(0)) {
-      // XCHECK-MESSAGES: :[[@LINE-1]]:13: warning: Conditional inside loop is ID-dependent due to ID function call [FPGA-ID-dependent-backward-branch]
-    }
-    j++;
-  } while (j < 256);
-  */
+    accumulator++;
+  } while (j < get_local_id(0));
+  // CHECK-NOTES: :[[@LINE-1]]:16: warning: Backward branch (do loop) is ID-dependent due to ID function call and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+  
+  for (int i = 0; i < tid; i++) {
+    // CHECK-NOTES: :[[@LINE-1]]:23: warning: Backward branch (for loop) is ID-dependent due to variable reference to 'tid' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
+  }
+
+  while (j < tid) {
+    // CHECK-NOTES: :[[@LINE-1]]:14: warning: Backward branch (while loop) is ID-dependent due to variable reference to 'tid' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
+  }
+
+  do {
+    accumulator++;
+  } while (j < tid);
+  // CHECK-NOTES: :[[@LINE-1]]:16: warning: Backward branch (do loop) is ID-dependent due to variable reference to 'tid' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+  
+  for (int i = 0; i < Example.IDDepField; i++) {
+    // CHECK-NOTES: :[[@LINE-1]]:23: warning: Backward branch (for loop) is ID-dependent due to member reference to 'IDDepField' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
+  }
+
+  while (j < Example.IDDepField) {
+    // CHECK-NOTES: :[[@LINE-1]]:14: warning: Backward branch (while loop) is ID-dependent due to member reference to 'IDDepField' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
+    accumulator++;
+  }
+
+  do {
+    accumulator++;
+  } while (j < tid);
+  // CHECK-NOTES: :[[@LINE-1]]:16: warning: Backward branch (do loop) is ID-dependent due to member reference to 'IDDepField' and may cause performance degradation [FPGA-ID-dependent-backward-branch]
 }
 
 void success() {
+  int accumulator = 0;
 
+  for (int i = 0; i < 1000; i++) {
+    if (i < get_local_id(0)) {
+      accumulator++;
+    }
+  }
 }
