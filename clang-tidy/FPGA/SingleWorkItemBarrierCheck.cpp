93155655a1472b8a061f52903969783edc3a9f15
Frank Wanye <wanyef@charon.cs.vt.edu>
2019-07-24 18:58:44 -0400

    Formatted max linewidth to 80 characters

--- clang-tidy/FPGA/SingleWorkItemBarrierCheck.cpp
+++ clang-tidy/FPGA/SingleWorkItemBarrierCheck.cpp
@@ -18,9 +18,11 @@ namespace tidy {
 namespace FPGA {
 
 void SingleWorkItemBarrierCheck::registerMatchers(MatchFinder *Finder) {
-  // Find any function that calls barrier but does not call either get_local_id or get_global_id, and will thus be treated as a single-work-item kernel
-  //hasAttr(attr::Kind::OpenCLKernel) restricts it to only kernel functions
-  // TODO: Have it accept all functions but check for a parameter that gets an ID from one of the two ID functions
+  // Find any function that calls barrier but does not call either get_local_id
+  // or get_global_id, and will thus be treated as a single-work-item kernel
+  // hasAttr(attr::Kind::OpenCLKernel) restricts it to only kernel functions
+  // TODO: Have it accept all functions but check for a parameter that gets an
+  // ID from one of the two ID functions
   Finder->addMatcher(
     //Find function declarations
     functionDecl(allOf(
@@ -45,22 +47,34 @@ void SingleWorkItemBarrierCheck::registerMatchers(MatchFinder *Finder) {
 }
 
 void SingleWorkItemBarrierCheck::check(const MatchFinder::MatchResult &Result) {
-  //TODO: Support reqd_work_group_size attribute (if it is anything other than (1,1,1) it will be interpreted as an NDRange (at least in 17.1, don't know about earlier)
+  //TODO: Support reqd_work_group_size attribute (if it is anything other than
+  // (1,1,1) it will be interpreted as an NDRange (at least in 17.1, don't know
+  // about earlier)
   const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("function");
   const auto *MatchedBarrier = Result.Nodes.getNodeAs<CallExpr>("barrier");
   if (aoc_version < 1701) {
-    diag(MatchedDecl->getLocation(), "Kernel function %0 does not call get_global_id or get_local_id and will be treated as single-work-item.\nBarrier call at %1 may error out")
-      << MatchedDecl
-      << MatchedBarrier->getBeginLoc().printToString(Result.Context->getSourceManager());
+    diag(MatchedDecl->getLocation(),
+         "Kernel function %0 does not call get_global_id or get_local_id and "
+         "will be treated as single-work-item.\nBarrier call at %1 may error "
+         "out")
+        << MatchedDecl
+        << MatchedBarrier->getBeginLoc().printToString(
+                Result.Context->getSourceManager());
   } else {
-    diag(MatchedDecl->getLocation(), "Kernel function %0 does not call get_global_id or get_local_id may be a viable single work-item kernel, but barrier call at %1 will force NDRange execution. If single work-item semantics are desired a mem_fence may be more efficient.")
-      << MatchedDecl
-      << MatchedBarrier->getBeginLoc().printToString(Result.Context->getSourceManager());
+    diag(MatchedDecl->getLocation(), 
+         "Kernel function %0 does not call get_global_id or get_local_id may "
+         "be a viable single work-item kernel, but barrier call at %1 will "
+         "force NDRange execution. If single work-item semantics are desired a "
+         "mem_fence may be more efficient.")
+        << MatchedDecl
+        << MatchedBarrier->getBeginLoc().printToString(
+                Result.Context->getSourceManager());
 
   }
 }
 
-void SingleWorkItemBarrierCheck::storeOptions(ClangTidyOptions::OptionMap &Opts) {
+void SingleWorkItemBarrierCheck::storeOptions(
+    ClangTidyOptions::OptionMap &Opts) {
   Options.store(Opts, "aoc_version", aoc_version);
 }
 
